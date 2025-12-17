3ab52b7ec1b7e19b565c8176590dec9bc799d578
Abdulsamad <100205577+Abdulsamad25@users.noreply.github.com>
2025-12-17 21:23:15 +0100

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -7,7 +7,7 @@ export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
   const currency = "₦";
-  const delivery_fee = 4000;
+  const delivery_fee = 3000;
   const backendUrl = import.meta.env.VITE_BACKEND_URL
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
